92d32176f8dd28b52f1a7503e18dc76beb88202a
Souza284 <kanaan.pedro2@gmail.com>
2024-12-09 21:51:58 -0300

    Exercise02 - Lists
    In this exercise, the user will input some numbers and will only stops when he decides to. When the user decides to stop, the program will output the ordered list.

---
+++ Exercise02/Program.cs
@@ -0,0 +1,46 @@
+﻿var numbers = new List<int>(); //list witch contains the numbers. obs: they can only be insert once
+
+while (true)
+{
+    Console.ForegroundColor = ConsoleColor.White; //It returns the text color to white
+    Console.WriteLine("Type in a number: ");
+    var number = Convert.ToInt32(Console.ReadLine()); 
+
+    if (numbers.Contains(number)) //if there is this number in the list, it won't be add again
+    {
+        Console.ForegroundColor = ConsoleColor.Red; // It changes the text color to red
+        Console.WriteLine("Duplicate number. I won't add it...");
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Green; // It changes the text color to green
+        Console.WriteLine("Number added successfully."); //if the is not this number in the list, it will be add
+        numbers.Add(number);
+    }
+
+    Console.ReadKey();
+
+    Console.ForegroundColor = ConsoleColor.White; //It returns the text color to white
+    Console.WriteLine("Do you want to stop the program? [Yes/No]:");
+    var flag = Console.ReadLine()!; //variable created to indicate if the user wants to stop the program or not
+    flag = flag.ToUpper();
+
+    if (flag == "YES")
+    {
+        break;
+    }
+    else
+    {
+        continue;
+    }
+}
+Console.ForegroundColor = ConsoleColor.White; //It returns the text color to white
+Console.Write("The list you have created is [ ");
+
+numbers.Sort();
+
+foreach(var number in numbers)
+{
+    Console.Write($"{number} ");
+}
+Console.Write("]");
