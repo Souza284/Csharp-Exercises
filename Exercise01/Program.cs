dbab1edf8cc31306e4fdcaaab5a3e0c82c7d6ed3
Souza284 <kanaan.pedro2@gmail.com>
2024-12-09 21:17:25 -0300

    Exercise01 - List
    I created a program that the user inputs 4 numbers and they go to a list. Then, it is shown the greatest and smallest numbers and their respectives positions in the list.

--- Exercise01/Program.cs
+++ Exercise01/Program.cs
@@ -1,1 +1,35 @@
-﻿Console.WriteLine("Hello, World");
+﻿var numbers = new List<int>(4);
+
+for (var i = 0; i < 4 ; i++)
+{
+    Console.WriteLine($"Number for position [{i}]:");
+    var num = Convert.ToInt32(Console.ReadLine());
+    numbers.Add(num);
+    //Console.ReadKey();
+
+}
+
+var maxNumber = numbers.Max();
+var minNumber = numbers.Min();
+Console.Write($"\nThe greatest number is {maxNumber} in the position ");
+
+for (var i = 0; i < numbers.Count; i++) 
+{
+    //Console.Write("-" + number + "-");
+    
+    if (numbers[i] == maxNumber)
+    {
+        Console.Write(i + "..");
+    }
+}
+Console.Write($"\nThe smallest number is {minNumber} in the position ");
+
+for (var i = 0; i < numbers.Count; i++) 
+{
+    //Console.Write("-" + number + "-");
+    
+    if (numbers[i] == minNumber)
+    {
+        Console.Write(i + "..");
+    }
+}
