12f229d304d68803b2af2d47785b1de5a15759a0
Souza284 <kanaan.pedro2@gmail.com>
2025-01-06 18:03:10 -0300

    .

---
+++ Exercise18/Program.cs
@@ -0,0 +1,8 @@
+﻿var numbers = new List<int> {1, 2, 19, 53, 56, 17, 5, 20, 102, 45};
+
+var evenNumber = numbers.Where(n => n % 2 == 1);
+
+foreach (var number in evenNumber)
+{
+    Console.WriteLine(number);
+}
