a3ba328d127ae7d559409ac53d0178550a41c0e8
Souza284 <kanaan.pedro2@gmail.com>
2025-01-07 10:09:45 -0300

    .

---
+++ Exercise22/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Enter in any word or frase: ");
+var word = Console.ReadLine()!;
+
+word = word.ToLower();
+word = word.Replace(" ", "");
+
+var letterList = new List<char>();
+
+foreach(char letter in word)
+{
+    letterList.Add(letter);
+}
+
+var _letterList = letterList.GroupBy(l => l, l => l);
+
+foreach(var letter in _letterList)
+{
+    Console.WriteLine("The letter " + letter.Key + " appeared " + letter.Count() + " times");
+}
