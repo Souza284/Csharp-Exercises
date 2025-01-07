0587ee189547a8fda6b399fe8b3c300af6a29fd1
Souza284 <kanaan.pedro2@gmail.com>
2025-01-07 15:05:37 -0300

    .

---
+++ Exercise23/Program.cs
@@ -0,0 +1,23 @@
+﻿var rnd = new Random();
+var numbers = new List<int>();
+
+for (var i = 0; i < 10; i++)
+{
+    var sortNumber = rnd.Next(1, 101);
+    numbers.Add(sortNumber);
+}
+
+foreach(var number in numbers)
+{
+    Console.Write(number + " ");
+}
+
+var greatNumbers = numbers.Where(n => n > 80).ToList();
+
+greatNumbers.Sort();
+Console.WriteLine("");
+
+foreach(var number in greatNumbers)
+{
+    Console.Write(number + " ");
+}
