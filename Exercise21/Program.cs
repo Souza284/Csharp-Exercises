a0b1be506f3bb52d81de1309824e08c6b9eacc23
Souza284 <kanaan.pedro2@gmail.com>
2025-01-07 08:34:17 -0300

    .

---
+++ Exercise21/Program.cs
@@ -0,0 +1,10 @@
+﻿var numbers = new List<int>{1, 5, 7, 1, 9, 10, 5, 7, 7};
+
+var repeatedNumbers = from n in numbers
+                    group n by n into y
+                    select y;
+
+foreach(var num in repeatedNumbers)
+{
+    Console.WriteLine("The number " + num.Key + " appears " + num.Count() + " times");
+}
