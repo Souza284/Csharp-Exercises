4e48d3e94c2b45a11f147616d213ae9b813d9adc
Souza284 <kanaan.pedro2@gmail.com>
2025-02-04 13:51:39 -0300

    .

---
+++ Exercise34/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var fileAddress = "binaryTest.txt";
+
+        using(var fileFlow = Console.OpenStandardInput())
+        using(var fileStream = new FileStream(fileAddress, FileMode.Create))
+        {
+            var buffer = new byte[1024];
+
+            while(true)
+            {
+                var readBytes = fileFlow.Read(buffer, 0, 1024);
+
+                fileStream.Write(buffer, 0, readBytes);
+                fileStream.Flush();
+
+                Console.WriteLine(readBytes);
+            }
+            
+        }
+    }
+}
