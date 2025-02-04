ba6756dcd95d06ddc39dff6b91834b3ab7c767b5
Souza284 <kanaan.pedro2@gmail.com>
2025-02-04 18:01:48 -0300

    .

---
+++ Exercise36/Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var fileAddress = "_test.txt";
+
+        File.WriteAllText(fileAddress, "Pedro Souza");
+    }
+}
