be46d5576c50cfa1686224a4f8147ae7efc4de4a
Souza284 <kanaan.pedro2@gmail.com>
2025-02-04 13:23:43 -0300

    .

---
+++ Exercise32/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var fileAddress = "Accounts.txt";
+
+        using(var fileFlow = new FileStream(fileAddress, FileMode.Open))
+        using(var reader = new StreamReader(fileFlow))
+        {
+            //var text = reader.ReadToEnd();
+
+            //var line = reader.ReadLine();
+
+            //Console.Write(text);
+            //Console.WriteLine(line);
+        }
+    }
+}
