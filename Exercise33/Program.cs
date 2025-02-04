695060ef02252577a45cf3968059c8f09e029a13
Souza284 <kanaan.pedro2@gmail.com>
2025-02-04 13:32:59 -0300

    .

---
+++ Exercise33/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var fileAddress = "test.txt";
+
+        using(var fileFlow = new FileStream(fileAddress, FileMode.Create))
+        using(var creator = new StreamWriter(fileFlow))
+        {
+            creator.WriteLine(false);
+            creator.WriteLine("Pedro/" + 18 + "/Student");
+            creator.WriteLine(90.2);
+        }
+    }
+}
